f445d72245956b64100b43f7f3c969622a7c60f2
Yahia <yahia@Yahias-MacBook-Pro.local>
2017-05-10 20:13:00 +0300

    [ Change ] SimpleGame to return Actual data

--- FallingWords/app/src/main/java/xom/xyz/fallingwords/design/model/SimpleGame.java
+++ FallingWords/app/src/main/java/xom/xyz/fallingwords/design/model/SimpleGame.java
@@ -1,43 +1,53 @@
 package xom.xyz.fallingwords.design.model;
 
 import java.util.List;
+import java.util.Random;
 
 import xom.xyz.fallingwords.DataAccessLayer.DataModels.WordItem;
+
 
 /**
  * Created by yahia on 5/8/17.
  */
 
 public class SimpleGame implements  GameModel{
-    List<WordItem> mWords;
+    WordItem word;
+    String proposedAnswer;
     public SimpleGame(List<WordItem> words) {
-        mWords = words;
+        Random r = new Random();
+
+        int randomWord = r.nextInt(words.size()-1);
+        int randomProposedAnswer = r.nextInt(words.size()-1);
+
+
+        word = words.get(randomWord);
+        proposedAnswer = words.get(randomProposedAnswer).getTextEng();
 
     }
 
     //All the Data will be brought from Data Access Layer
     @Override
     public String getQuestion() {
-        return null;
+        return word.getTextSpa();
     }
 
     @Override
     public String getRightAnswer() {
-        return null;
+        return word.getTextEng();
     }
 
     @Override
     public String getProposedAnswer() {
-        return null;
+        return proposedAnswer;
     }
 
     @Override
     public int getScore() {
-        return 0;
+        return 1;
     }
 
     @Override
     public Long getTime() {
-        return null;
+        return 30L;
     }
 }
